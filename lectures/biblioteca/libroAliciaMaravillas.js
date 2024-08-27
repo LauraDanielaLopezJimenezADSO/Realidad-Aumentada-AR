bee00600bc9f5e751c46b5cee0db2c8db7e8a655
Laura Daniela Lopez Jimenez <lauralopezjimenez12@gmail.com>
2024-08-27 10:15:40 -0500

    trabajo del martes

--- lectures/biblioteca/libroAliciaMaravillas.js
+++ lectures/biblioteca/libroAliciaMaravillas.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targetsnuevos/targetsAlicia.mind',
+      imageTargetSrc: '../../assets/targetsnuevos/targetsAlicias.mind',
       maxTrack: 4,
     });
     const {renderer, scene, camera} = mindarThree;
@@ -14,60 +14,49 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
 
     //primer modelo 
-    const conejo = await loadGLTF('../../assets/models/libroAliciaMaravilllas/aliciaConejo.glb');
-    conejo.scene.scale.set(5, 5, 5);
+    const conejo = await loadGLTF('../../assets/models/libroAliciaMaravilllas/aliciaConejo.glb'); 
+    conejo.scene.scale.set(0.1, 0.1, 0.1);
     conejo.scene.position.set(0, 0, 0);
 
     //segundo modelo 
-    const portada = await loadGLTF('../../assets/models/libroAliciaMaravilllas/momentoTe.glb');
-    portada.scene.scale.set(0.4, 0.4, 0.4);
-    portada.scene.position.set(0, 0, 0);
+    const bear = await loadGLTF('../../assets/models/libroAliciaMaravilllas/momentoTe.glb');  
+    bear.scene.scale.set(0.1, 0.1, 0.1);
+    bear.scene.position.set(0, 0, 0);
 
     
     //tercer modelo 
-    const reinaCorazones = await loadGLTF('../../assets/models/libroAliciaMaravilllas/reinaCorazones.glb');
-    reinaCorazones.scene.scale.set(0.5, 0.5, 0.5);
-    reinaCorazones.scene.position.set(0, 0, 0);
+    const raccoon = await loadGLTF('../../assets/models/libroAliciaMaravilllas/reinaCorazones.glb'); 
+    raccoon.scene.scale.set(0.1, 0.1, 0.1);
+    raccoon.scene.position.set(0, 0, 0);
 
     //cuarto modelo 
-    const sombrerero = await loadGLTF('../../assets/models/libroAliciaMaravilllas/sombrerero.glb');
-    sombrerero.scene.scale.set(0.5, 0.5, 0.5);
+    const sombrerero = await loadGLTF('../../assets/models/libroAliciaMaravilllas/sombrerero.glb'); 
+    sombrerero.scene.scale.set(2, 2, 2);
     sombrerero.scene.position.set(0, 0, 0);
-
     
     //0 representa la primera imagen 
     const conejoAnchor = mindarThree.addAnchor(0);
     conejoAnchor.group.add(conejo.scene);
     const mixerconejo = new THREE.AnimationMixer(conejo.scene);
-    //0 controla la animacion del modelo
-    
 
     //1 representa la segunda imagen 
-    const portadaAnchor = mindarThree.addAnchor(1);
-    portadaAnchor.group.add(portada.scene);
-    const mixer = new THREE.AnimationMixer(portada.scene);
-    // const action = mixer.clipAction(portada.animations[0]);
-    // action.play();
+    const bearAnchor = mindarThree.addAnchor(1);
+    bearAnchor.group.add(bear.scene);
+    const mixer = new THREE.AnimationMixer(bear.scene);
 
     //2 representa la terceera imagen 
-    const reinaCorazonesAnchor = mindarThree.addAnchor(2);
-    reinaCorazonesAnchor.group.add(reinaCorazones.scene);
-    const mixer1 = new THREE.AnimationMixer(reinaCorazones.scene);
+    const raccoonAnchor = mindarThree.addAnchor(2);
+    raccoonAnchor.group.add(raccoon.scene);
+    const mixer1 = new THREE.AnimationMixer(raccoon.scene);
 
-    // //3 representa la primera imagen 
+    //2 representa la terceera imagen 
     const sombrereroAnchor = mindarThree.addAnchor(3);
     sombrereroAnchor.group.add(sombrerero.scene);
-    const mixersombrerero = new THREE.AnimationMixer(sombrerero.scene);
-    
-
-
-    // //0 controla la animacion del modelo
-    const action1 = mixer1.clipAction(reinaCorazones.animations[0]);
-    action1.play();
+    const mixerSombrerero = new THREE.AnimationMixer(sombrerero.scene);
    
     const clock = new THREE.Clock();
     await mindarThree.start();
-    renderer.setAnimationLsombrererop(() => {
+    renderer.setAnimationLoop(() => {
       const delta = clock.getDelta();
       mixer.update(delta);
       
@@ -75,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       mixerconejo.update(delta);
 
-      mixersombrerero.update(delta);
+      mixerSombrerero.update(delta)
      
       renderer.render(scene, camera);
     });
@@ -83,5 +72,3 @@ document.addEventListener('DOMContentLoaded', () => {
   start();
   
 });
-
-
